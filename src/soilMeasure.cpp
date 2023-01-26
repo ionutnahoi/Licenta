837cf3b0590b64170cd456da7194bfada33110e5
Ionut Nahoi <ioan.nahoi02@e-uvt.ro>
2023-01-26 21:01:36 +0200

    soil moisture
    -soil moisture sensor configuration
    -humidity and temperature sensor configuration
    -deleted useless code

--- src/soilMeasure.cpp
+++
@@ -1,8 +0,0 @@
-#include <soilMeasure.h>
-#include <Arduino.h>
-#define sensorPin 2
-int readSensor() {
-	int value=digitalRead(sensorPin);
-    Serial.print("Soil sensor value: ");
-    Serial.print(value);
-}
